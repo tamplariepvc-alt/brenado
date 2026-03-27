ea0f0d6ec793ec16d823a2f0c56d318017bd7e12
Mihai Preda <m009mihai@gmail.com>
2026-03-27 15:10:33 +0200

    filtre noWiwpw

--- app/page.js
+++ app/page.js
@@ -2804,51 +2804,7 @@ const filteredTasks = tasks.filter((task) => {
                 />
               </div>
 <div className="mt-4 flex flex-wrap justify-center gap-3 pb-1">
-<button
-  onClick={() => setStatusFilter("Noua")}
-  className={`relative rounded-2xl px-5 py-3 text-sm font-semibold ${
-    statusFilter === "Noua"
-      ? "bg-blue-600 text-white"
-      : "bg-blue-50 text-blue-700"
-  }`}
->
-  Noua
-
-  <span className="absolute -top-2 -right-2 flex h-6 min-w-[24px] items-center justify-center rounded-full bg-slate-500 px-1 text-xs font-bold text-white">
-    {formatCount(countNoua)}
-  </span>
-</button>
-
-<button
-  onClick={() => setStatusFilter("In lucru")}
-  className={`relative rounded-2xl px-5 py-3 text-sm font-semibold ${
-    statusFilter === "In lucru"
-      ? "bg-orange-500 text-white"
-      : "bg-orange-50 text-orange-700"
-  }`}
->
-  In lucru
-
-  <span className="absolute -top-2 -right-2 flex h-6 min-w-[24px] items-center justify-center rounded-full bg-slate-500 px-1 text-xs font-bold text-white">
-    {formatCount(countInLucru)}
-  </span>
-</button>
-
-<button
-  onClick={() => setStatusFilter("Finalizata")}
-  className={`relative rounded-2xl px-5 py-3 text-sm font-semibold ${
-    statusFilter === "Finalizata"
-      ? "bg-green-600 text-white"
-      : "bg-green-50 text-green-700"
-  }`}
->
-  Finalizata
-
-  <span className="absolute -top-2 -right-2 flex h-6 min-w-[24px] items-center justify-center rounded-full bg-slate-500 px-1 text-xs font-bold text-white">
-    {formatCount(countFinalizata)}
-  </span>
-</button>
-
+ {["Noua", "In lucru", "Finalizata"].map((status) => {
     const isActive = statusFilter === status;
 
     let activeClass = "bg-slate-900 text-white";
