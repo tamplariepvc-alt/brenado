3ba822f87431dc0a32c904d0102a91adc3c7dd6b
Mihai Preda <m009mihai@gmail.com>
2026-04-02 10:35:37 +0300

    managerss

--- app/page.js
+++ app/page.js
@@ -3504,7 +3504,7 @@ const filteredTasks = tasks.filter((task) => {
     <p className="text-xs font-semibold text-slate-900">
       {(profile?.full_name || "Utilizator") +
         " - " +
-        (profile?.role === "admin" || profile?.role === "manager" ? "(Administrator)" : "(Manager)" : "(Utilizator)")}
+        (profile?.role === "admin" || profile?.role === "manager" ? "(Administrator)" : "(Utilizator)")}
     </p>
   </div>
 </header>
